8d30558face39ae98e6aff1f8d3011358bd05aa7
darshandeepak-07 <darshandeepak1483@gmail.com>
2025-02-05 21:01:57 +0530

    added reverse hash for little endian

--- utils/helper.go
+++ utils/helper.go
@@ -8,5 +8,12 @@ import (
 func GenerateHash(data string) string {
 	byteData := []byte(data)
 	hash := sha256.Sum256(byteData)
-	return hex.EncodeToString(hash[:])
+	return hex.EncodeToString(reverse(hash[:]))
+}
+
+func reverse(bytes []byte) []byte {
+	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+	}
+	return bytes
 }
